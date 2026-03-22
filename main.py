7d52b7fa8992b1c29921dd74cd1d72dbbc8b7de4
asyncfunction <13813298288@139.com>
2026-03-22 14:22:56 +0800

    合并乱码了

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
 from gui import App
 import tkinter as tk
 
 root = tk.Tk()
 App(root)
 root.mainloop()
-=======
-"""
-单位换算器 - 主入口
-启动程序
-"""
-from converter_gui import create_gui
-
-
-if __name__ == "__main__":
-    create_gui()
->>>>>>> 004a9208a5964b26d8fec900de1339cba22f2173
